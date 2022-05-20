181a29c1ea1e85e3192ce1b6d7ec32b52dc3cc81
Alexander Zhdanov <77617952+exceedololo@users.noreply.github.com>
2022-05-20 19:13:37 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,13 +1,13 @@
 #pragma once
-#include "transport_catalogue.h"
-#include <utility>
+#include "transport_catalogue.h"    
+#include <utility>          
 #include <string>
-#include <string_view>
-#include <istream>
-#include <iostream>
+#include <string_view>      
+#include <istream>          
+#include <iostream>         
 #include <vector>
 
-namespace transport_catalogue::detail {
+namespace transport_catalogue::detail{
     std::pair<std::string_view, std::string_view> Split(std::string_view, char, int count = 1);
 
     std::string_view Lstrim(std::string_view);
@@ -17,11 +17,11 @@ namespace transport_catalogue::detail {
     std::string_view TrimString(std::string_view);
 }
 
-namespace transport_catalogue::input_reader {
-    void ProcessInput(TransportCatalogue& tc, std::istream& is);
-    void ProcessInputQueries(TransportCatalogue& tc, std::vector<InputQuery>& queries);
+namespace transport_catalogue::input_reader{
+    void ProcessInput(TransportCatalogue&, std::istream&);
+    void ProcessInputQueries(TransportCatalogue&, std::vector<InputQuery>&);
 
-    Stop ProcessQueryAddStop(std::string& query);
-    void ProcessQueryAddStopsDistance(TransportCatalogue& tc, std::string& query);
-    Bus ProcessQueryAddRoute(TransportCatalogue& tc, std::string& query);
+    Stop ProcessQueryAddStop(std::string&);
+    void ProcessQueryAddStopsDistance(TransportCatalogue&, std::string&);
+    Bus ProcessQueryAddRoute(TransportCatalogue&, std::string&);
 }
