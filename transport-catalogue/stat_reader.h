2b0f9de4a524a62af811d97127c3616c2cf7e8fe
Alexander Zhdanov <77617952+exceedololo@users.noreply.github.com>
2022-05-20 09:19:54 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -12,7 +12,7 @@
 namespace transport_catalogue::stat_reader{
     std::ostream& operator<<(std::ostream&, const Stop*);
     std::ostream& operator<<(std::ostream& os, const Route* route);
-
+/////////
     std::ostream& ProcessRequests(std::ostream&, TransportCatalogue&, std::istream&);  
     std::ostream& ExecuteRequest(std::ostream&, TransportCatalogue&, RequestQuery&); 
 } 
